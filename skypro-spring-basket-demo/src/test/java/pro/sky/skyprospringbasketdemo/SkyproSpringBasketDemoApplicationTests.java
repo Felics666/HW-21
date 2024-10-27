4147ea037caea2f9a8150763189ed88eea1f57a9
Felics666 <felics6@gmail.com>
2024-10-27 18:41:54 +0300

    Done

---
+++ skypro-spring-basket-demo/src/test/java/pro/sky/skyprospringbasketdemo/SkyproSpringBasketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringbasketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringBasketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
